b41011d4162d0af0d9405dafe5c7612830e99d78
Jordan Gonzales <jgonzo127@gmail.com>
2014-10-15 21:17:59 -0700

    added new code for counting turns and recording number guesses

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
 
 $(document).ready(function(){
 	
+	var random = Math.floor(Math.random() * 101);
+	
+	$('#guessButton').click(function(e) {
+		e.preventDefault();
+		var userChoice = $('#userGuess').val();
+		var $span = $('#count')
+		if (userChoice == "") {
+			alert('Please input a number!');
+			return;
+		} else {
+			$('#guessList').append('<li>' + userChoice + '</li>');
+			}
+			$span.text(Number($span.text()) + 1);
+		})
+/*	for (i = 1; i<= userChoice; i++) {
+	if ( userChoice != 0) {
+        alert('warmer!');
+			} else if (userChoice > random) {
+    
+            } else if (userChoice = random) {
+
+			} else 
+
+    	}
+	}
+};
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
-  	});
+  	})
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
-  	});
+  	})
+
+	$('.new').click(function() {
+		location.reload();
+	});
+	  
 
 });
 
