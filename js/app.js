9fee4cb2bbe86669af54ef301856fa9a035ee8e4
Jordan Gonzales <jgonzo127@gmail.com>
2014-10-16 13:00:01 -0700

    refined javascript further

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function(){
-	
+//generates random number
 	var random = Math.floor(Math.random() * 101);
-	
+	console.log(random);
+//takes user guess input
 	$('#guessButton').click(function(e) {
 		e.preventDefault();
 		var userGuess = $('#userGuess').val();
 		var $span = $('#count')
+//checks if input is 100 or less and a number
 		if (userGuess == "" || (isNaN(userGuess))) {
 			alert('Please input a number from 1-100!');
 			return;
@@ -14,22 +16,21 @@ $(document).ready(function(){
 			alert("Number must be 100 or less!");
 			return;
 		}
+//adds guessed number to the list
 		$('#guessList').append('<li>' + userGuess + '</li>');
+//records how many guesses to user has made
 		$span.text(Number($span.text()) + 1);
-		$('#userGuess').val('');
+//compares user guess to random 
+		if (userGuess == random) {
+			$('#feedback').replaceWith('<h2 id ="feedback">You guessed the number! It was ' + random +'! It took you ' + $span.text() + ' tries!</h2>');
+		} else if (userGuess > random) {
+			$('#feedback').replaceWith('<h2 id ="feedback">Too high! Guess lower</h2>');
+		} else if (userGuess < random) {
+			$('#feedback').replaceWith('<h2 id ="feedback">Too low! Guess higher</h2>');
+		}
 		})
-/*	for (i = 1; i<= userGuess; i++) {
-	if ( userGuess != 0) {
-        alert('warmer!');
-			} else if (userGuess > random) {
-    
-            } else if (userGuess = random) {
 
-			} else 
 
-    	}
-	}
-};
 
 
 	/*--- Display information modal box ---*/
@@ -42,7 +43,7 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	})
-
+//reloads page on new game
 	$('.new').click(function() {
 		location.reload();
 	});
